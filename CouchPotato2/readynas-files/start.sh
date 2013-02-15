6d792e35edd56e3e49afc8d6a8ac4d80aa372982
Tobias Hieta <tobias@hieta.se>
2013-02-15 14:20:23 +0000

    Add couchpotato

---
+++ CouchPotato2/readynas-files/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/apps/couchpotato/CouchPotato.py --quiet --daemon --pid_file=/apps/couchpotato/couchpotato.pid --config_file=/apps/couchpotato/config.ini --data_dir=/apps/couchpotato/datadir
