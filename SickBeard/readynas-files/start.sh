585a333295f493c0870e4f2defade44cd34d5431
Tobias Hieta <tobias@hieta.se>
2013-02-15 13:20:27 +0000

    Add sickbeard

---
+++ SickBeard/readynas-files/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/apps/sickbeard/SickBeard.py -q --daemon --nolaunch --pidfile=/apps/sickbeard/sickbeard.pid --datadir=/apps/sickbeard/datadir --port=5052 --nolaunch --config=/apps/sickbeard/config.ini
